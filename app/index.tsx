27254f3b0b65edaec300eb58f954c330b96fc69d
Adnan <adbak.dev@gmail.com>
2024-06-30 21:24:57 +0200

    init

--- app/index.tsx
+++ app/index.tsx
@@ -6,17 +6,26 @@ import { useWindowDimensions, View } from "react-native";
 export default function Index() {
   const { width, height } = useWindowDimensions();
 
-  // Use the image for background
   const bg = useImage(require("../assets/sprites/background-day.png"));
   const bird = useImage(require("../assets/sprites/yellowbird-upflap.png"));
+  const pipe = useImage(require("../assets/sprites/pipe-green.png"));
+  const pipeReverse = useImage(require("../assets/sprites/pipe-green-reverse.png"));
+  const base = useImage(require("../assets/sprites/base.png"));
+
+  const pipeOffset = 1000;
 
   return (
     <>
-      {/* Ensure the View component takes up all available space and no padding is applied */}
       <View style={{ flex: 1, paddingTop: 0 }}>
         <Canvas style={{ width, height }}>
           <Image image={bg} width={width} height={height} fit="cover" />
-          <Image image={bird} y={height / 2} x={120} width={64} height={48} fit="contain" />
+
+          <Image image={pipe} y={height - 320} x={width / 2} width={103} height={640} />
+          <Image image={pipeReverse} y={-320} x={width / 2} width={103} height={640} />
+
+          <Image image={base} y={height - 75} width={width} height={100} x={0} fit="fill" />
+
+          <Image image={bird} y={height / 2 - 24} x={width / 2 - 32} width={64} height={48} fit="contain" />
         </Canvas>
       </View>
     </>
